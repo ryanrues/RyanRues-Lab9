2f0b99085fb6522ad7c1710968e5f20341c8cc90
ryanrues <ryanrues0112@gmail.com>
2024-04-13 17:56:09 -0400

    Ryan Rues's
    I created an encode function that encodes the chosen password and returns it as a new variable.
    
    I also created a main() and print_menu() function that provides a looping menu until the user quits  and an if statement that will be used to call the created decode function.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 #this is Ryan Rues's main.py file
 
-def encode(password)
-    password = password
+def encode(password):
+    encodedDigits = [str(int(digit) + 3) for digit in password if digit.isdigit()]
+    encodedPassword = ''.join(encodedDigits)
+    return encodedPassword
 
-def print_menu()
-    
+def print_menu():
+    print("Menu \n------------- \n1. Encode \n2. Decode\n3. Quit")
+    selection = int(input("Please enter an option: "))
+    return selection
+
+def main():
+    while True:
+        selection = print_menu()
+        if selection == 1:
+            password = str(input("Please enter your password: "))
+            encodedPass = encode(password)
+            print("Your password has been encoded and stored!")
+        elif selection == 2:
+            break
+        elif selection == 3:
+            break
+        else:
+            print("Invalid selection")
+
+
+
+if __name__ == "__main__":
+    main()
